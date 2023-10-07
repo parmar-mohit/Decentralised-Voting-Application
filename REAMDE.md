a05ed904b927d57b2c21d2e3267e24ee9cb0bf46
parmar-mohit <mohitparmar8080@gmail.com>
2023-10-07 19:36:10 +0530

    Initial Commit

---
+++ REAMDE.md
@@ -0,0 +1,3 @@
+## Decentralised Voting Application
+
+This project aims to develop a decentralised voting application using blockchain technology. Users can import their wallet on to the platform and start using it to conduct election or vote for a candidate in live election process.
---
+++ contracts/Election.sol
@@ -0,0 +1,91 @@
+pragma solidity ^0.8.0;
+
+contract Election{
+    mapping ( string => uint) private candidatesVotes;
+    string[] candidatesList;
+    string electionName;
+    bool electionStart;
+    bool electionEnd;
+    address owner;
+
+    constructor(string memory election){
+        electionStart = false;
+        electionEnd = false;
+        electionName = election;
+        owner = msg.sender;
+    }
+
+    function startElection() external returns(bool){
+        if( msg.sender == owner ){
+            electionStart = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    function endElection() external returns(bool){
+        if( msg.sender == owner ){
+            electionEnd = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    function isElectionRunning() view external returns(bool){
+        if( electionStart == true && electionEnd == false ){
+            return true;
+        }
+
+        return false;
+    }
+
+    function addCandidate(string memory candidateName) external returns(bool){
+        if( electionStart == false && msg.sender == owner ){
+            candidatesVotes[candidateName] = 0;
+            candidatesList.push(candidateName);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function voteCandidate(string memory candidateName) external returns(bool){
+        if( electionStart == true && electionEnd == false ){
+            candidatesVotes[candidateName] += 1;
+
+            return true; 
+        }
+
+        return false;
+    }
+
+    function getCandidates() external view returns(string[] memory){
+        return candidatesList;
+    }
+
+    function getVotes() external view returns(string[] memory, uint[] memory) {
+        uint256[] memory votes= new uint256[](candidatesList.length);
+        for( uint i = 0; i < candidatesList.length; i++ ){
+            votes[i] = candidatesVotes[candidatesList[i]];
+        }
+
+        return(candidatesList,votes);
+    }
+
+    function getWinner() external view returns(string memory){
+        string memory winner = candidatesList[0];
+        uint votes = candidatesVotes[winner];
+
+        for( uint i = 1; i < candidatesList.length; i++ ){
+            if( votes < candidatesVotes[candidatesList[i]] ){
+                votes = candidatesVotes[candidatesList[i]];
+                winner = candidatesList[i];
+            }
+        }
+
+        return winner;
+    }
+}
